961ed50224f2fd87c11b413baf390d9e31cad339
MinepixlFRA <minepixlyt@gmail.com>
2025-04-10 17:29:01 +0200

    index redumentaire termine

--- js.js
+++ js.js
@@ -1,6 +1,8 @@
 const headerBtn = document.getElementById("header-btn");
 const nav = document.querySelector("nav");
+const bgNav = document.getElementById("close-nav");
 
 function toggleNav() {
 	nav.classList.toggle("hidden");
+	bgNav.classList.toggle("hidden");
 }
